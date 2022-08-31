2113ae947a2247c4ece23a56b357f15020e385a4
Lucas Silveira Prazeres <lprazeres@gmail.com>
2022-08-31 15:01:44 -0300

    Alteração na página About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ export function About() {
                 <div className={styles.borderB}>
                     <img className={styles.aboutLogo} src={logoArsenalHeader}></img>
                     <p>
-                        Fundada em 2006, somos a maior torcida organizado do Arsenal da Palhoça. Nossa missão é apoiar o Arsenal nas vitórias (que sejam muitas), ou nas derrotas.
+                        Fomos fundada em 2006, somos a maior torcida organizado do Arsenal da Palhoça. Nossa missão é apoiar o Arsenal nas vitórias (que sejam muitas), ou nas derrotas.
                     </p>
                     <p>
                         Nos encontramos todas as semanas no bar do seu Ori, Jardim Aquários, e ficamos TPM (Totalmente Pirados e Malucos), enquanto apoiamos o maior de Londres.
